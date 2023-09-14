5389972f0bafac8f9c0c57ad129e908e1ed58c99
Osman Dirican <osmnfdrcn@gmail.com>
2023-09-14 21:05:47 +0300

    refactor: home

--- components/modules/home/index.tsx
+++ components/modules/home/index.tsx
@@ -1,31 +1,12 @@
 "use client";
 import Card from "@/components/common/card";
-import Loader from "@/components/layout/loader";
 import SearchInput from "@/components/layout/page-header/search-input";
 import Spinner from "@/components/layout/spinner";
 import Banner from "@/components/modules/home/banner";
-import { IPlace } from "@/types";
-import { useEffect, useMemo, useState } from "react";
+import useHomePage from "./useHomePage";
 
 const Home = () => {
-  const [places, setPlaces] = useState<IPlace[] | null>(null);
-  const [isLoading, setIsloading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  useEffect(() => {
-    setIsloading(true);
-    fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/place/`, {
-      cache: "no-cache",
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setPlaces(res);
-      })
-      .catch(() => {
-        setIsError(true);
-      })
-      .finally(() => setIsloading(false));
-  }, []);
-
+  const { places, isLoading } = useHomePage();
   return (
     <section className=" w-full">
       <Banner />
